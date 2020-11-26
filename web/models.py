03c355f84a89b9802502529aaf0db83a04e9899d
H.Kiani <Kiani.hamidreza@gmail.com>
2020-11-26 20:41:11 +0330

    add Expens, Income model.

--- web/models.py
+++ web/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Expens(models.Model):
+    desc = models.CharField("شرح هزینه", max_length=200)
+    amount = models.DecimalField("مبلغ", max_digits=20, decimal_places=2)
+
+    def __str__(self):
+        return self.desc
+
+
+class Income(models.Model):
+    desc = models.CharField("شرح درآمد", max_length=200)
+    amount = models.DecimalField("مبلغ درآمد", max_digits=20, decimal_places=2)
+    
+    def __str__(self):
+        return self.desc
