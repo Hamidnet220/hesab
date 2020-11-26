fbefebd052c886cad1a919bb6aef52047be054c0
H.Kiani <Kiani.hamidreza@gmail.com>
2020-11-26 20:57:37 +0330

    add date field to Expens and Income Model.

--- web/models.py
+++ web/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Expens(models.Model):
     desc = models.CharField("شرح هزینه", max_length=200)
     amount = models.DecimalField("مبلغ", max_digits=20, decimal_places=2)
+    date = models.DateField("تاریخ")
+
 
     def __str__(self):
         return self.desc
@@ -11,6 +13,10 @@ class Expens(models.Model):
 class Income(models.Model):
     desc = models.CharField("شرح درآمد", max_length=200)
     amount = models.DecimalField("مبلغ درآمد", max_digits=20, decimal_places=2)
+    date = models.DateField("تاریخ")
     
+    class Meta:
+        order_with_respect_to = 'date'
+        
     def __str__(self):
         return self.desc
