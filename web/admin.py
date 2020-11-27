d172f4269b55f18315ef92966f40037c021bc938
H.Kiani <Kiani.hamidreza@gmail.com>
2020-11-27 12:47:42 +0330

    add BankAccount and join Incomes,Expens to it.

--- web/admin.py
+++ web/admin.py
@@ -1,25 +1,41 @@
 from django.contrib import admin
-from .models import Expens, Income, IncomeCategory, ExpensCategory
+from .models import BankAccount, Expens, Income, IncomeCategory, ExpensCategory
 
 admin.site.register(ExpensCategory)
 admin.site.register(IncomeCategory)
 
 
-def format_to_thous_sep(obj):
-    return '{:,}'.format(round(obj.amount,0))
-format_to_thous_sep.short_description = "مبلغ (ریال)"
 
 
 @admin.register(Expens)
 class ExpensAdmin(admin.ModelAdmin):
-    list_display = ('category', 'desc', format_to_thous_sep, 'date',)
+    list_display = ('category', 'desc', 'format_to_thous_sep', 'date',)
     list_filter = ('category',)
     ordering = ['-date'] 
+
+    def format_to_thous_sep(self, obj):
+        return '{:,}'.format(round(obj.amount,0))
+    format_to_thous_sep.short_description = "مبلغ (ریال)"
+
 
 
 @admin.register(Income)
 class IncomeAdmin(admin.ModelAdmin):
-    list_display = ('category', 'desc', format_to_thous_sep, 'date',)
+    list_display = ('category', 'desc', 'format_to_thous_sep', 'date',)
     list_filter = ('category',)
     ordering =['-date']
 
+    def format_to_thous_sep(self, obj):
+        return '{:,}'.format(round(obj.amount,0))
+    format_to_thous_sep.short_description = "مبلغ (ریال)"
+
+
+
+@admin.register(BankAccount)
+class BankAccountAdmin(admin.ModelAdmin):
+    list_display = ('bank_name', 'branch_name', 'account_number','format_to_thous_sep')
+    ordering =['bank_name']
+
+    def format_to_thous_sep(self, obj):
+        return '{:,}'.format(round(obj.current_balance,0))
+    format_to_thous_sep.short_description = "موجودی (ریال)"
--- web/models.py
+++ web/models.py
@@ -1,5 +1,25 @@
 from django.db import models
 
+BANKS=[
+    (1,"انصار"),
+    (2,"سامان"),
+    (3,"تجارت"),
+    (4,"مسکن"),
+    (5,"ملت"),
+]
+class BankAccount(models.Model):
+    bank_name = models.IntegerField(u"نام بانک", choices=BANKS)
+    account_number = models.CharField(u"شماره حساب", max_length=50, )
+    sheba_number = models.CharField(u"شماره شبا", max_length=26, default='IR', null=True, blank=True)
+    branch_name = models.CharField(u"نام شعبه", max_length=50, )
+    initial_balance = models.DecimalField(u"موجودی اولیه ", max_digits=20, decimal_places = 2, default=0)
+    current_balance = models.DecimalField(u"موجودی", max_digits=20, decimal_places=2, default=0)
+    desc = models.CharField(u"توضیحات", max_length=50, null=True, blank=True)
+
+    def __str__(self):
+        return ("%s" % (self.account_number))
+
+
 class ExpensCategory(models.Model):
     title = models.CharField(u"عنوان هزینه", max_length=100,)
     desc = models.CharField(u"توضیحات" ,max_length=200, null=True, blank=True)
@@ -12,8 +32,10 @@ class ExpensCategory(models.Model):
     def __str__(self):
         return self.title
 
+
 class Expens(models.Model):
     category = models.ForeignKey(ExpensCategory, verbose_name="گروه هزینه", on_delete=models.CASCADE)
+    from_bank_account = models.ForeignKey(BankAccount, verbose_name="برداشت از حساب", on_delete=models.CASCADE)
     desc = models.CharField("شرح هزینه", max_length=200)
     amount = models.DecimalField("مبلغ", max_digits=20, decimal_places=2)
     date = models.DateField("تاریخ")
@@ -22,6 +44,13 @@ class Expens(models.Model):
         order_with_respect_to = 'date'
         verbose_name = "هزینه"
         verbose_name_plural = "هزینه ها"
+
+    def save(self, *args, **kwargs):
+        bank_account = BankAccount.objects.get(pk=self.from_bank_account.id)
+        bank_account.current_balance -= self.amount
+        bank_account.save()
+        
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.desc
@@ -42,6 +71,7 @@ class IncomeCategory(models.Model):
 
 class Income(models.Model):
     category = models.ForeignKey(IncomeCategory, verbose_name = "گروه درآمد", on_delete=models.CASCADE,)
+    from_bank_account = models.ForeignKey(BankAccount, verbose_name="واریز به حساب", on_delete=models.CASCADE)
     desc = models.CharField("شرح درآمد", max_length=200)
     amount = models.DecimalField("مبلغ درآمد", max_digits=20, decimal_places=2)
     date = models.DateField("تاریخ")
