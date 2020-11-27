eee7dbfbcd7250414f51316d0be9c1e6613f7b18
H.Kiani <Kiani.hamidreza@gmail.com>
2020-11-27 07:59:36 +0330

    change list view of Expense and Incomes ,format thousand seperator of amounts in admin page.

--- web/admin.py
+++ web/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import Expens, Income, IncomeCategory, ExpensCategory
 
-admin.site.register(Expens)
-admin.site.register(Income)
 admin.site.register(ExpensCategory)
 admin.site.register(IncomeCategory)
 
+
+def format_amount_to_thous_sep(obj):
+    return '{:,}'.format(round(obj.amount,0))
+format_amount_to_thous_sep.short_description = "مبلغ (ریال)"
+
+
+@admin.register(Expens)
+class ExpensAdmin(admin.ModelAdmin):
+    list_display = ('category', 'desc', format_amount_to_thous_sep, 'date',)
+
+
+@admin.register(Income)
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('category', 'desc', format_amount_to_thous_sep, 'date',)
