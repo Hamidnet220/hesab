8b686844808265866b9f89b18323358e80b0b662
H.Kiani <Kiani.hamidreza@gmail.com>
2020-12-01 10:29:04 +0330

    add humanize to installed app.

--- hesab/settings.py
+++ hesab/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'jalali_date',
     'web',
 ]
@@ -111,7 +112,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
