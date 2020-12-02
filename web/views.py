da79ea14ff25c0f233c3bdb0a1c40c2d12bd49eb
H.Kiani <Kiani.hamidreza@gmail.com>
2020-12-02 19:47:28 +0330

    move html logic of show report bank_account to templates file.'

--- web/views.py
+++ web/views.py
@@ -1,19 +1,33 @@
+from django.db.models import Sum, F
+
 from django.shortcuts import render
-from .models import BankAccount,Expens
 from django.http import HttpResponse
 
-def get_bank_account_report(request, bank_acc_id):
+from .models import BankAccount,Expens
+
+
+
+def get_bank_account_report(request, bank_acc_id = 1):
+    
+    Expens.objects.annotate(total_amounts = Sum(F('amount')))
     expenses = Expens.objects.filter(from_bank_account__id = bank_acc_id).order_by('-date')
-    expens_str = ""
-    sum_amoun = 0
-    expens_str += str(BankAccount.objects.get(id = bank_acc_id))+ '<br>'
 
-    for expens in expenses:
-        expens_str += expens.desc +" | "+ '{:,}'.format(round(expens.amount),0) +" | "+ str(expens.date) + '<br>'
-        sum_amoun += expens.amount
+    bank_account = BankAccount.objects.get(id=bank_acc_id)
 
-    expens_str += "جمع کل"+ " | "+ '{:,}'.format(sum_amoun) + '<br>'
+    contents = {
+        'expenses' : expenses,
+        'bank_account' : bank_account,
+    }
 
-    html = '<html><body>%s</body></html> ' % expens_str
+    return render(request,"bank_account_report.html", contents)
 
-    return HttpResponse(html)
+
+def transfer_amount(request):
+
+    bank_accounts = BankAccount.objects.all()
+    
+
+    contents = {
+        'bank_accounts' : bank_accounts,
+    }
+    return render(request, "transfer_amount.html", contents)
