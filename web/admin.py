0e13fb2bf9b2a0fe7168506f75d40b003d140114
H.Kiani <Kiani.hamidreza@gmail.com>
2020-12-03 00:03:46 +0330

    remove deleted model Expens, Income, ExpensCategory, IncomeCategory from admin site.

--- web/admin.py
+++ web/admin.py
@@ -4,11 +4,9 @@ from jalali_date.admin import ModelAdminJalaliMixin
 
 from jalali_date import date2jalali
 
-from .models import BankAccount, Expens, Income, IncomeCategory, ExpensCategory, Transaction, TransCategory
+from .models import BankAccount, Transaction, TransCategory
 
 
-admin.site.register(ExpensCategory)
-admin.site.register(IncomeCategory)
 admin.site.register(TransCategory)
 
 
@@ -18,55 +16,39 @@ convert_date_to_jalali.short_description = "تاریخ"
 
 @admin.register(Transaction)
 class TransactionAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ['bank_account','description','amount','trans_category', convert_date_to_jalali,
+    list_display = ['bank_account','description','format_to_thous_sep','trans_category', convert_date_to_jalali,
     'transaction_type']
+    ordering = ['-date'] 
     list_filter=['bank_account','trans_category']
 
-@admin.register(Expens)
-class ExpensAdmin(ModelAdminJalaliMixin, admin.ModelAdmin):
-    list_display = ('category', 'desc', 'format_to_thous_sep', convert_date_to_jalali,)
-    list_filter = ('category',)
-    ordering = ['-date'] 
-    actions = ['delete_expens']
+    actions = ['delete_trans']
 
     def format_to_thous_sep(self, obj):
         return '{:,}'.format(round(obj.amount,0))
     format_to_thous_sep.short_description = "مبلغ (ریال)"
 
-    def delete_expens(self, request, queryset):
+
+    def delete_trans(self, request, queryset):
         for obj in queryset:
-            bank_acc = obj.from_bank_account
-            bank_acc.current_balance += obj.amount
+            bank_acc = obj.bank_account
+            if obj.transaction_type == 1:
+                bank_acc.current_balance -= obj.amount
+            elif obj.transaction_type == 2:
+                 bank_acc.current_balance += obj.amount
             bank_acc.save()
             obj.delete()
-    delete_expens.short_description = "حذف هزینه"
+    delete_trans.short_description = "حذف تراکنش"
 
-
-@admin.register(Income)
-class IncomeAdmin(admin.ModelAdmin):
-    list_display = ('category', 'desc', 'format_to_thous_sep', convert_date_to_jalali,)
-    list_filter = ('category',)
-    ordering =['-date']
-    actions = ['delete_income']
-    search_field = ['desc']
-
-    def format_to_thous_sep(self, obj):
-        return '{:,}'.format(round(obj.amount,0))
-    format_to_thous_sep.short_description = "مبلغ (ریال)"
 
     def save_model(self, request, obj, form, change):
         bank_account = obj.to_bank_account
-        bank_account.current_balance += obj.amount
+        if obj.transaction_type == 1:
+            bank_acc.current_balance += obj.amount
+        elif obj.transaction_type == 2:
+            bank_acc.current_balance -= obj.amount
+
         bank_account.save()
         super().save_model(request, obj, form, change)
-
-    def delete_income(self, request, queryset):
-        for obj in queryset:
-            bank_acc = obj.to_bank_account
-            bank_acc.current_balance -= obj.amount
-            bank_acc.save()
-            obj.delete()
-    delete_income.short_description = 'حذف درآمد'
 
 
 @admin.register(BankAccount)
--- web/models.py
+++ web/models.py
@@ -29,6 +29,8 @@ class BankAccount(models.Model):
             return ("%s" % (self.get_bank_name_display())) # کیف پول
         else:
             return ("%s" % (self.account_number))
+
+            
 TRANS_CATEGROY_TYPE =[
     (1,"هزینه"),
     (2,"درآمد"),
@@ -69,68 +71,3 @@ class Transaction(models.Model):
     def __str__(self):
         return self.description
     
-
-class ExpensCategory(models.Model):
-    title = models.CharField(u"عنوان هزینه", max_length=100,)
-    desc = models.CharField(u"توضیحات" ,max_length=200, null=True, blank=True)
-
-    class Meta:
-        order_with_respect_to = 'title'
-        verbose_name = "عنوان هزینه"
-        verbose_name_plural = "عناوین هزینه ها"
-
-    def __str__(self):
-        return self.title
-
-
-class Expens(models.Model):
-    category = models.ForeignKey(ExpensCategory, verbose_name="گروه هزینه", on_delete=models.CASCADE)
-    from_bank_account = models.ForeignKey(BankAccount, verbose_name="برداشت از حساب", on_delete=models.CASCADE)
-    desc = models.CharField("شرح هزینه", max_length=200)
-    amount = models.DecimalField("مبلغ", max_digits=20, decimal_places=2)
-    date = models.DateField("تاریخ", default=utils.timezone.now)
-    
-    class Meta:
-        order_with_respect_to = 'date'
-        verbose_name = "هزینه"
-        verbose_name_plural = "هزینه ها"
-
-    def save(self, *args, **kwargs):
-        bank_account = BankAccount.objects.get(pk=self.from_bank_account.id)
-        bank_account.current_balance -= self.amount
-        bank_account.save()
-
-        super().save(*args, **kwargs)
-
-    def __str__(self):
-        return self.desc
-
-
-class IncomeCategory(models.Model):
-    title = models.CharField(u"عنوان درآمد", max_length=100,)
-    desc = models.CharField(u"توضیحات" ,max_length=200, null=True, blank=True)
-
-    class Meta:
-        order_with_respect_to = 'title'
-        verbose_name = "عنوان درآمد"
-        verbose_name_plural = "عناوین درآمدها"
-
-    def __str__(self):
-        return self.title
-
-
-class Income(models.Model):
-    category = models.ForeignKey(IncomeCategory, verbose_name = "گروه درآمد", on_delete=models.CASCADE,)
-    to_bank_account = models.ForeignKey(BankAccount, verbose_name="واریز به حساب", on_delete=models.CASCADE)
-    desc = models.CharField("شرح درآمد", max_length=200)
-    amount = models.DecimalField("مبلغ درآمد", max_digits=20, decimal_places=2)
-    date = models.DateField("تاریخ", default=utils.timezone.now)
-    
-    class Meta:
-        order_with_respect_to = 'date'
-        verbose_name = "درآمد"
-        verbose_name_plural = "درآمد ها"
-
-    def __str__(self):
-        return self.desc
-
