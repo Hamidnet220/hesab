8f1c8d04bbe4ba164a2114a3f641ad764ea02b04
H.Kiani <Kiani.hamidreza@gmail.com>
2020-12-07 00:27:52 +0330

    set name for urls in web app.

--- web/urls.py
+++ web/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bank_report/<int:bank_acc_id>', views.get_bank_account_report),
-    path('category_report', views.get_category_expens_amount),
+    path('bank_report/<int:bank_acc_id>', views.get_bank_account_report, name='bank_report'),
+    path('category_report', views.get_category_expens_amount, name='category_report'),
+    
 ]
