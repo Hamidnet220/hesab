d59303a7ec0b09848af2cd9a63b336763ef66020
H.Kiani <Kiani.hamidreza@gmail.com>
2020-12-20 00:16:31 +0330

    change url of bank_report to query by form request and remove requier id in url.

--- web/urls.py
+++ web/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('bank_report/<int:bank_acc_id>', views.get_bank_account_report, name='bank_report'),
+    path('bank_report/', views.get_bank_account_report, name='bank_report'),
     path('category_report', views.get_category_expens_amount, name='category_report'),
     
 ]
