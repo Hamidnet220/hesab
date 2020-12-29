eba54cd9ac4455f05d5cdb32ad641419ac771174
kiani.hamidreza <Kiani.hamidreza@gmail.com>
2020-12-29 06:45:59 +0330

    remove unused lib in forms module.

--- web/forms.py
+++ web/forms.py
@@ -1,9 +1,6 @@
 from django import forms
 from .models import Transaction
 
-from jalali_date.fields import JalaliDateField, SplitJalaliDateTimeField
-from jalali_date.widgets import AdminJalaliDateWidget, AdminSplitJalaliDateTime
-
 class AddTransactionForm(forms.ModelForm):
     class Meta:
         model = Transaction
